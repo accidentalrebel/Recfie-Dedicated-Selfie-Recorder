992eaa166490f8feb49c8c85de374f2075f3d883
accidentalrebel <accidentalrebel@gmail.com>
2019-10-30 09:50:09 +0800

    Can now sort the files.

--- recfie.py
+++ recfie.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import os, shutil, subprocess
+import os, shutil, subprocess, sys
 from datetime import datetime
 
 USB_PATH = "/run/media/arebel/2004-1014/DCIM/DCIMA/"
@@ -10,7 +10,12 @@ QUEUE_FILE_PATH = "/home/arebel/videos/recfie/conversion-queue.txt"
 
 source_path = TEMP_USB_PATH
 
-files_to_process = os.listdir(source_path)
+def get_sorted_files(path):
+    files = os.listdir(path)
+    files.sort()
+    return files
+
+files_to_process = get_sorted_files(source_path)
 
 print("Converting files...")
 for f in files_to_process:
@@ -21,7 +26,7 @@ for f in files_to_process:
 
 print("\nConcatenating files...")
 concat_string = ""
-for f in os.listdir(UNPROCESSED_PATH):
+for f in get_sorted_files(UNPROCESSED_PATH):
     concat_string += "file '" + UNPROCESSED_PATH + f + "'\n"
 
 concat_string = concat_string[0:len(concat_string) - 1]
@@ -42,6 +47,6 @@ print('Removing files in ' + UNPROCESSED_PATH)
 for f in os.listdir(UNPROCESSED_PATH):
     os.remove(UNPROCESSED_PATH + f)
 
-# print('Removing files in ' + source_path)
-# for f in os.listdir(source_path):
-#     os.remove(source_path + f)
+print('Removing files in ' + source_path)
+for f in os.listdir(source_path):
+    os.remove(source_path + f)
